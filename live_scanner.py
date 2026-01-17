137846847c49c46636beb0478828869080384118
ajaywilson <41744654+ajaywilson@users.noreply.github.com>
2026-01-17 11:47:41 +0530

    Update live_scanner.py

--- live_scanner.py
+++ live_scanner.py
@@ -7,10 +7,13 @@ from config import *
 def scan(symbol):
     df = fetch_data(symbol, INTERVAL, PERIOD_LIVE)
     if df.empty or len(df) < 50:
-        return
+        return False
 
     df = add_indicators(df).dropna()
 
     if check_signal(df, len(df)-1):
-        price = df.iloc[-1]["Close"]
+        price = float(df.iloc[-1]["Close"])
         send_message(f"📈 Signal: {symbol} at ₹{round(price,2)}")
+        return True
+
+    return False

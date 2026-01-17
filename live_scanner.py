18200c991c44b173b47f7768630fb82e92fb09a8
ajaywilson <41744654+ajaywilson@users.noreply.github.com>
2026-01-17 10:49:30 +0530

    Update live_scanner.py

--- live_scanner.py
+++ live_scanner.py
@@ -2,15 +2,15 @@ from data import fetch_data
 from indicators import add_indicators
 from strategy import check_signal
 from telegram_bot import send_message
-from config import INTERVAL, PERIOD_LIVE
+from config import *
 
 def scan(symbol):
     df = fetch_data(symbol, INTERVAL, PERIOD_LIVE)
-
     if df.empty or len(df) < 50:
         return
 
     df = add_indicators(df).dropna()
 
     if check_signal(df, len(df)-1):
-        send_message(f"📈 Intraday Signal detected on {symbol}")
+        price = df.iloc[-1]["Close"]
+        send_message(f"📈 Signal: {symbol} at ₹{round(price,2)}")

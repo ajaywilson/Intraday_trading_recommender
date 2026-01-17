090b32effe8e80f7cc8aa2e37007653fdef479e4
ajaywilson <41744654+ajaywilson@users.noreply.github.com>
2026-01-17 10:48:36 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,4 @@
-import os
-import requests
+import os, requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
@@ -8,9 +7,13 @@ def send_message(text):
     if not BOT_TOKEN or not CHAT_ID:
         print(text)
         return
-
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, data={
-        "chat_id": CHAT_ID,
-        "text": text
-    })
+    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+
+def send_photo(path, caption=None):
+    if not BOT_TOKEN or not CHAT_ID:
+        return
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
+    with open(path, "rb") as img:
+        requests.post(url, files={"photo": img},
+                      data={"chat_id": CHAT_ID, "caption": caption})

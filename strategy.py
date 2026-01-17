dac12c42ecb9fa0f37ac525d2068498881040304
ajaywilson <41744654+ajaywilson@users.noreply.github.com>
2026-01-17 11:04:51 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,11 +1,20 @@
 def check_signal(df, i):
     row = df.iloc[i]
-    prev_high = df["High"].iloc[i-11:i-1].max()
+
+    close = float(row["Close"])
+    open_ = float(row["Open"])
+    vwap = float(row["VWAP"])
+    ema9 = float(row["EMA9"])
+    ema20 = float(row["EMA20"])
+    volume = float(row["Volume"])
+    vol_avg = float(row["Vol_Avg"])
+
+    prev_high = float(df["High"].iloc[i-11:i-1].max())
 
     return (
-        row["Close"] > row["VWAP"] and
-        row["EMA9"] > row["EMA20"] and
-        row["Close"] > row["Open"] and
-        row["Volume"] > 1.5 * row["Vol_Avg"] and
-        row["Close"] > prev_high
+        close > vwap and
+        ema9 > ema20 and
+        close > open_ and
+        volume > 1.5 * vol_avg and
+        close > prev_high
     )

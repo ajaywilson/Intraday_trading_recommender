770972d59107b1407d3186e3cc415ff9d2db201d
ajaywilson <41744654+ajaywilson@users.noreply.github.com>
2026-01-17 12:30:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
+# config.py
+
 CAPITAL = 3000
 RISK_PER_TRADE = 0.01
 
+# 👉 CHANGE ONLY THIS when you finalize best RR
+RR = 1.75
+
+# Used only for research table
+RR_OPT_VALUES = [1.0, 1.25, 1.5, 1.75, 2.0]
+
 INTERVAL = "5m"
 PERIOD_BACKTEST = "15d"
-PERIOD_LIVE = "5d"
 
 MARKET_OPEN = (9, 15)
 MARKET_CLOSE = (15, 30)

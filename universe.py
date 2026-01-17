3e76d2f48da3b43354f9a875111ed5038a6db30b
ajaywilson <41744654+ajaywilson@users.noreply.github.com>
2026-01-17 11:10:16 +0530

    Update universe.py

--- universe.py
+++ universe.py
@@ -1,6 +1,20 @@
 import pandas as pd
+import yfinance as yf
 
 def get_universe():
     url = "https://archives.nseindia.com/content/indices/ind_nifty50list.csv"
     df = pd.read_csv(url)
-    return [s + ".NS" for s in df["Symbol"].tolist()]
+    symbols = [s + ".NS" for s in df["Symbol"].tolist()]
+
+    valid = []
+
+    for sym in symbols:
+        try:
+            test = yf.download(sym, period="1d", progress=False)
+            if not test.empty:
+                valid.append(sym)
+        except:
+            continue
+
+    print(f"Loaded {len(valid)} valid symbols out of {len(symbols)}")
+    return valid
